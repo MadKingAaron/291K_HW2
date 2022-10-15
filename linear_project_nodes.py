cd7f7dde73da4cb6c850a2b5dafac0889e6e9925
Aaron Jimenez <arjl5012@gmail.com>
2022-10-14 20:43:18 -0700

    Added calc for all layers

--- linear_project_nodes.py
+++ linear_project_nodes.py
@@ -1,16 +1,18 @@
 
-s=1
-k=5
-w=32
-p=0
+initial_size = 32
+
+layers = [{'s':1, 'k':5, 'p':0}, {'s':2, 'k':2, 'p':0}, {'s':1, 'k':5, 'p':0}, {'s':2, 'k':2, 'p':0}]
+
+
 
 def size(w, k, p, s):
     return ((w-k+2*p)/s)+1
 
 
-s=2
-k=2
-w=10
-p=0
+def get_final_size(layers, w):
+    for layer in layers:
+        w = size(w, layer['k'], layer['p'], layer['s'])
+    return w
 
-print(size(w,k,p,s))
+
+print(get_final_size(layers, initial_size))
