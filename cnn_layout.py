4de48e8e7f1e47eba3c013dc39667d0c104ed2bd
Aaron Jimenez <55520679+MadKingAaron@users.noreply.github.com>
2022-10-23 14:45:53 -0700

    Added ability to save best model

--- cnn_layout.py
+++ cnn_layout.py
@@ -3,6 +3,8 @@ from torch import max_pool2d, nn, relu, optim
 from torchvision import datasets, transforms
 from tqdm import tqdm
 import numpy as np
+import GoogleNet_Arz
+import json
 
 # checking if GPU is available or not
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -55,6 +57,39 @@ class CNN_Layout1(nn.Module):
     def forward(self, x):
         y = self.model(x)
         return y
+
+class CNN_Layout2(nn.Module):
+    def __init__(self) -> None:
+        super(CNN_Layout2, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1), # 1
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), # 2
+
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1), # 3
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), # 4
+
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1, padding=1), # 5
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1), # 6
+            nn.Dropout(p=0.2),
+
+
+            nn.Flatten(),
+            nn.Linear(120*2*2, 120), #Check input number
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10)
+            #nn.Softmax()
+
+        )
+
+    def forward(self, x):
+        y = self.model(x)
+        return y
+
 
 def train(epoch, model, trainloader, criterion, optimizer):
     model.train()
@@ -169,16 +204,79 @@ def test_training_and_val():
     ds = get_dataset()
     ds = transform_dataset(ds['train_ds'], ds['valid_ds'], ds['test_ds'])
     loaders = get_loaders(ds['train_ds'], ds['valid_ds'], ds['test_ds'])
-    print(loaders)
+    #print(loaders)
     train_for_nepoch(epochs, model, loaders['train_loader'], criterion, optimizer)
     validate(epochs-1, model, loaders['valid_loader'], criterion)
 
-modelArch_list = [CNN_Layout1]
+
+# Test different combinations
+
+
+modelArch_list = [CNN_Layout1, CNN_Layout2]
 optimizer_list = [optim.SGD, optim.Adam]
 loss_list = [nn.CrossEntropyLoss]
+#optimizer = [optim.SGD]
+lrs = [0.001, 0.002, 0.003, 0.01, 0.02, 0.03]
+epochs = [10, 20, 30, 40, 50, 60, 70]
+
+    
+
+def train_and_val_all_combo():
+    ds = get_dataset()
+    ds = transform_dataset(ds['train_ds'], ds['valid_ds'], ds['test_ds'])
+    loaders = get_loaders(ds['train_ds'], ds['valid_ds'], ds['test_ds'])
+    best_model_hyperpram = None
+    best_model = None
+    for modelArch in modelArch_list:
+        for optimizerFunc in optimizer_list:
+            for loss_func in loss_list:
+                for epoch in epochs:
+                    for lr in lrs:
+                        print("Class:", modelArch.__name__, "Optimizer:", optimizerFunc.__name__, "LR:", lr, "Loss:", loss_func.__name__, "Epoch", epoch)
+                        print("\nTraining:")
+                        model = modelArch().to(device)
+                        criterion = loss_func().to(device)
+                        optimizer = optimizerFunc(model.parameters(), lr=lr)
+                        train_for_nepoch(epoch, model, loaders['train_loader'], criterion, optimizer)
+                        print("\nValidating:")
+                        val_values = validate(epoch-1, model, loaders['valid_loader'], criterion)
+                        
+                        # Save best model performance
+                        if best_model_hyperpram is None or best_model_hyperpram['accuracy'] < val_values['accuracy']:
+                            best_model_hyperpram = {"class":modelArch.__name__, "optimizer":optimizerFunc.__name__, "lr":lr, "loss_func":loss_func.__name__, "epochs":epoch, "accuracy":val_values['accuracy'], 'total_loss':val_values['loss']}
+                            best_model = model
+                        print("\n\n\n")
+    with open("best_model.json", "w") as outfile:
+        json.dump(best_model_hyperpram, outfile, indent=4)
+    torch.save(best_model, "best_model.pth")
+    
 
 
 #get_dataset()
 #print(CNN_Layout1().to(device))
 
-test_training_and_val()
+#test_training_and_val()
+def test_trainingGoog():
+    model = GoogleNet_Arz.GoogleNet().to(device)
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    epochs = 2
+
+    ds = get_dataset()
+    ds = transform_dataset(ds['train_ds'], ds['valid_ds'], ds['test_ds'])
+    loaders = get_loaders(ds['train_ds'], ds['valid_ds'], ds['test_ds'])
+    print(loaders)
+    train_for_nepoch(epochs, model, loaders['train_loader'], criterion, optimizer)
+    validate(epochs-1, model, loaders['valid_loader'], criterion)
+def tester():
+    ds = get_dataset()
+    ds = transform_dataset(ds['train_ds'], ds['valid_ds'], ds['test_ds'])
+    loaders = get_loaders(ds['train_ds'], ds['valid_ds'], ds['test_ds'])
+    for data in loaders['train_loader']:
+        print(data)
+        print(data[0].shape)
+        input()
+
+#print(optim.SGD.__name__)
+train_and_val_all_combo()
+#test_trainingGoog()
